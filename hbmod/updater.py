9b3eca4575bfcc08a3c6fd3ae5f985c754a1fbf9
James <jmaccrimmon@icloud.com>
2023-08-11 15:04:41 -0600

    Update to version 1.4.4

--- hbmod/updater.py
+++ hbmod/updater.py
@@ -8,3 +8,6 @@ unix_time = time.time() * 1000
 changelog = input("Comments? ")
 
 json.dump({"latest-version": ver, "update-time-unix": unix_time, "changelog": changelog}, open("ver.json", "w"))
+
+os.system("git commit -a -m \"Update to version " + ver + "\"")
+os.system("git push")
